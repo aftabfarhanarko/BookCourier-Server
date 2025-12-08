1b449d0db9c38ffea3dc5a16b153a3a1ebc579b7
Aftab Farhan <aftabfarhan324@gmail.com>
2025-12-08 12:26:00 +0600

    start server now

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Project Creat");
+import "dotenv/config"; 
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = Number(process.env.START_PORT) || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World start Book Library Server");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
